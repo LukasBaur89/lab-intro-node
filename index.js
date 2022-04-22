dc5206f6db682a65b047d22ec1069c69f5b6dd01
Lukas Baur <baurlukas89@gmail.com>
2022-04-22 15:52:36 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
+// create sorted list of numbers
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    // push value to array
+    const pushItem = this.items.push(item);
+    // sort array in ascending order
+    const sortItems = this.items.sort((a, b) => a - b);
+    // update new length
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    // index pos, this.items.length;
+    if (pos <= this.items.length) {
+      return pos;
+    }
+    if (pos > this.items.length - 1) {
+      throw new Error("OutOfBonds");
+    }
+  }
 
-  max() {}
+  max() {
+    // Math.max, empty = 0 => throw new Error
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    // Math.min
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    // if list = 0
+    // reduce, edge case: sortedList empty
+    if (this.items.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((a, b) => a + b);
+  }
 
-  avg() {}
+  avg() {
+    const avg = this.items.reduce((a, b) => a + b) / this.items.length;
+    return avg;
+  }
 }
 
 module.exports = SortedList;
